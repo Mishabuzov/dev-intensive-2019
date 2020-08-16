c98a64cc8b59379cd6e9edf2839e5f5f45baa49a
Mishabuzov <mikhaijl7@gmail.com>
2020-08-16 13:07:40 +0300

    added Utils with String processing functions

---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,22 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        val parts: List<String>? = fullName?.split(" ")
+
+        val firstName = parts?.getOrNull(0)
+        val lastName = parts?.getOrNull(1)
+        return firstName to lastName
+    }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        TODO()
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? = when {
+        firstName.isNullOrBlank() && lastName.isNullOrBlank() -> null
+        !firstName.isNullOrBlank() && lastName.isNullOrBlank() -> firstName[0].toString()
+        firstName.isNullOrBlank() && !lastName.isNullOrBlank() -> lastName[0].toString()
+        else -> "${firstName?.get(0)}${lastName?.get(0)}"
+    }
+}
