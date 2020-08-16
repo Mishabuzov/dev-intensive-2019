4a6f8ac8103fc8446c01941c2c40e7e89df6381d
Mishabuzov <mikhaijl7@gmail.com>
2020-08-16 13:30:22 +0300

    implemented translit function

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,5 +1,7 @@
 package ru.skillbranch.devintensive.utils
 
+import java.lang.StringBuilder
+
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
         val parts: List<String>? = fullName?.split(" ")
@@ -10,7 +12,50 @@ object Utils {
     }
 
     fun transliteration(payload: String, divider: String = " "): String {
-        TODO()
+        val translitDict = mapOf(
+            'а' to "a",
+            'б' to "b",
+            'в' to "v",
+            'г' to "g",
+            'д' to "d",
+            'е' to "e",
+            'ё' to "e",
+            'ж' to "zh",
+            'з' to "z",
+            'и' to "i",
+            'й' to "i",
+            'к' to "k",
+            'л' to "l",
+            'м' to "m",
+            'н' to "n",
+            'о' to "o",
+            'п' to "p",
+            'р' to "r",
+            'с' to "s",
+            'т' to "t",
+            'у' to "u",
+            'ф' to "f",
+            'х' to "h",
+            'ц' to "c",
+            'ч' to "ch",
+            'ш' to "sh",
+            'щ' to "sh'",
+            'ъ' to "",
+            'ы' to "i",
+            'ь' to "",
+            'э' to "e",
+            'ю' to "yu",
+            'я' to "ya"
+        )
+        val translit = StringBuilder()
+        for (letter in payload) {
+            if (letter in translitDict) {
+                translit.append(translitDict[letter])
+            } else {
+                translit.append(letter)
+            }
+        }
+        return translit.toString().replace(" ", divider)
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? = when {
